1aa98f7c01d0057127acb7b49e5e65f90030ad7c
nicanico <nicsousan@gmail.com>
2023-07-06 20:41:22 -0300

    06/07 - testagem de componentes (Button Load More) e verificação com coverage

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -1,6 +1,7 @@
 
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { Button } from '.'
+import userEvent from '@testing-library/user-event'
 
 describe('<Button />', () => {
     /*
@@ -24,5 +25,53 @@ describe('<Button />', () => {
         expect(button).toBeInTheDocument()
         expect(button).toHaveAttribute('class', 'button')
     })
+
+    it('should call function on button click', () =>{
+        // jest gera uma função propria
+        const fn = jest.fn()
+        // criado um onclick e passamos a fn
+        render(<Button text="Load more" onClick={fn}/>)
+        const button = screen.getByRole('button', { name: /load more/i })
+
+        // aciona click no botão atraves do fireEvent
+        userEvent.click(button) // usar userEvent torna mais natural
+        // fireEvent.click(button)
+        // fireEvent.click(button)
+
+        // verifica se a função foi chamada
+        expect(fn).toHaveBeenCalled()
+        // espera quantidade exata de chamadas através da função
+        expect(fn).toHaveBeenCalledTimes(1)
+
+    })
+
+    it('should call function on button click', () =>{
+
+        render(<Button text="Load more" disabled={true}/>)
+
+        const button = screen.getByRole('button', { name: /load more/i })
+
+        // checa se o botão está desativado
+        expect(button).toBeDisabled()
+
+    })
+
+    it('should call function on button click', () =>{
+
+        render(<Button text="Load more" disabled={false}/>)
+
+        const button = screen.getByRole('button', { name: /load more/i })
+
+        // checa se o botão está ativado
+        expect(button).toBeEnabled()
+
+    })
+
+    /**
+        para checar se ainda a testes a serem feitos podemos utilizar o comando:
+        npm test -- --coverage
+        ele irá verificar os locais que ainda faltam testes para serem executos
+     */
+
 })
 
